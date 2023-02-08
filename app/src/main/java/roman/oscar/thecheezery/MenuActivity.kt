9fa987d68d6828399284d7ab1bf29815db75ee16
oscarm8 <oscar.roman229303@potros.itson.edu.mx>
2023-02-08 00:15:47 -0700

    Commit del proyecto

---
+++ app/src/main/java/roman/oscar/thecheezery/MenuActivity.kt
@@ -0,0 +1,12 @@
+package roman.oscar.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+    }
+}
