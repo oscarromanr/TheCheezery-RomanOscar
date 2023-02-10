69f39511de60a5de4832dba792b3f57c6f5d6e97
oscarm8 <oscar.roman229303@potros.itson.edu.mx>
2023-02-09 22:51:04 -0700

    Agregado listViews

--- app/src/main/java/roman/oscar/thecheezery/MenuActivity.kt
+++ app/src/main/java/roman/oscar/thecheezery/MenuActivity.kt
@@ -1,12 +1,42 @@
 package roman.oscar.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+        var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+        var btnHot: Button = findViewById(R.id.button_hot_drinks) as Button
+        var btnSweets: Button = findViewById(R.id.button_sweets) as Button
+        var btnSalties: Button = findViewById(R.id.button_salties) as Button
+
+        btnCold.setOnClickListener{
+            var intent: Intent =Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipoProducto", "coldDrinks")
+            startActivity(intent)
+        }
+
+        btnHot.setOnClickListener{
+            var intent: Intent =Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipoProducto", "hotDrinks")
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener{
+            var intent: Intent =Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipoProducto", "salties")
+            startActivity(intent)
+        }
+
+        btnSweets.setOnClickListener{
+            var intent: Intent =Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipoProducto", "sweets")
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/roman/oscar/thecheezery/Product.kt
@@ -0,0 +1,6 @@
+package roman.oscar.thecheezery
+
+data class Product (var name: String,
+                    var image: Int,
+                    var description: String,
+                    var price: Double)
